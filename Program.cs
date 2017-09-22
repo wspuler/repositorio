12691dd14bde2cbaa3fe62c094e88e30186149e6
Walter Spuler <wspuler@micracel.com.ar>
2017-09-22 14:12:30 -0300

    primer version

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsolaCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string valor = "hola";
+            valor += " mundo";
+
+            string valor2= Resolver(valor);
+            
+            Console.WriteLine(valor2);
+
+        }
+
+         public static string Resolver(string parametro)
+        {
+            return parametro + " !!";
+        }
+    }   
+
+}
